8f387a7e08f0924666139b232b15217ed325e04d
v.yarashchuk <v.yarashchuk@godeltech.com>
2021-09-17 13:16:36 +0300

    init core alg implementation

---
+++ Caesar/Caesar/Core/CaesarCipherProcessor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Text;
+
+namespace Caesar.Core
+{
+    public class CaesarCipherProcessor
+    {
+        private readonly Func<int, int, int> sum = (a, b) => a + b;
+
+        private readonly Func<int, int, int> substract = (a, b) => a - b;
+
+        public string Encrypt(string input, int key)
+        {
+            return ApplyCaesarCipherToString(input, key, sum);
+        }
+
+        public string Decrypt(string input, int key)
+        {
+            return ApplyCaesarCipherToString(input, key, substract);
+        }
+
+        private string ApplyCaesarCipherToString(string input, int key, Func<int, int, int> operation)
+        {
+            var result = new StringBuilder(input.Length);
+            foreach (var character in input)
+            {
+                result.Append(ApplyCaesarCipherToCharacter(character, key, operation));
+            }
+            return result.ToString();
+        }
+
+        private char ApplyCaesarCipherToCharacter(char input, int key, Func<int, int, int> operation)
+        {
+            var alphabetBeginning = char.IsUpper(input) ? 'A' : 'a';
+
+            var result = (operation(input, key) - alphabetBeginning) % 26 + alphabetBeginning;
+
+            return (char)result;
+        }
+
+
+    }
+}
---
+++ Caesar/Caesar/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Caesar.Core;
+
+namespace Caesar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var proc = new CaesarCipherProcessor();
+            var input = "ABCDEF";
+            Console.WriteLine($"encrypted: {proc.Encrypt(input, 2)}");
+        }
+    }
+}
